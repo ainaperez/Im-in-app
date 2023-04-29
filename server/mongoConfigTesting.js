8c587da9e4fd9392c397b844c01df519df76d5ec
Andrew Pearson <andrewtpearson94@gmail.com>
2023-04-29 16:13:24 +0100

    Created mongoConfigTesting file for general testing; some changes in unit test files

--- server/mongoConfigTesting.js
+++ server/mongoConfigTesting.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const { MongoMemoryServer } = require("mongodb-memory-server");
 
+
 async function initializeMongoServer() {
   const mongoServer = await MongoMemoryServer.create();
   const mongoUri = mongoServer.getUri();
