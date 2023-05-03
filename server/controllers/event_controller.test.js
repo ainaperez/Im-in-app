cc2b3fd07ca0df899f6e75550d824cfdd57164b5
mirzaalihussain2 <mirzaalihussain2@gmail.com>
2023-05-03 23:07:50 +0100

    One test for POSTING events working

--- server/controllers/event_controller.test.js
+++ server/controllers/event_controller.test.js
@@ -1,56 +1,55 @@
-const mongoose = require('mongoose');
-const { MongoMemoryServer } = require('mongodb-memory-server');
-const { MongoClient } = require('mongodb');
-//let mongod = null;
+// global imports
+const request = require("supertest");
 
-// This test fails because 1 !== 2
-// it("Testing to see if Jest works", () => {
-//   expect(1).toBe(2);
-// });
+// local imports
+const mongoose = require("../db");
+const app = require("../index");
+const Event = require("../models/event_model");
 
-// // This test passes because 1 === 1
-// it("Testing to see if Jest works", () => {
-//   expect(1).toBe(1);
-// });
-
-describe('Initial test', function () {
-  let connection;
-  let mongoServer;
-
-  beforeAll(
-    async function () {
-      mongoServer = await MongoMemoryServer.create();
-      connection = await MongoClient.connect(mongoServer.getUri(), {});
-    }
-  );
-
-  afterAll(
-    async function () {
-      if (connection) {await connection.close()};
-      if (mongoServer) {await mongoServer.stop()};
-    }
-  );
-
-  it('should successfully set & get information from the database', async function () {
-    const db = connection.db(mongoServer.instanceInfo.dbName);
-    expect(db).toBeDefined();
-    const collection = db.collection('test');
-    const result = await collection.insertMany([{ a: 1 }, { b: 1 }]);
-    expect(result.insertedCount).toStrictEqual(2);
-    expect(await collection.countDocuments({})).toBe(2);
-  });
+/* Connecting to the DB before each test. */
+beforeAll(async () => {
+  await Event.deleteMany({});
 });
 
-// beforeAll or beforeEach?
+/* Connecting DB connection after each test. */
+afterAll(async () => {
+  await mongoose.connection.close();
+});
 
-// // This will create an new instance of "MongoMemoryServer" and automatically start it
-// const mongod = await MongoMemoryServer.create();
+/* TEST CASE | HAPPY PATH: Get all events from DB, return nothing */
+// describe("HAPPY PATH: Get all events from DB, return nothing", () => {
+//   it("returns all events, currently none", async () => {
+//     const res = await request(app).get("/events");
+//     expect(res.statusCode).toBe(201);
+//     expect(res.body.length).toBe(0);
+//   })
+// });
 
-// const uri = mongod.getUri();
-
-// // The Server can be stopped again with
-// await mongod.stop();
-
-
-
+/* TEST CASE | HAPPY PATH: Add an event to DB correctly */
+describe("HAPPY PATH: Add an event to DB correctly", () => {
+  it("creates first event", async () => {
+    const res = await request(app).post("/add-event").send({
+      owner: "64415e6c97483b215fd4c3ee",
+      title: "Yoga",
+      description: "Join us for a peaceful morning of yoga in the park. All levels welcome!",
+      date: "2023-05-01T10:00:00.000Z",
+      location: "Central Park, New York",
+      coordinates: [
+        40.785091,
+        -73.968285
+      ],
+      image: "event1-mock_bjhoxl.jpg",
+      limitAttendees: 20,
+      invitees: [],
+      hideFrom: [],
+      joined: [],
+      announcements: [],
+      canceled: false,
+      active: true,
+      liked: false
+    });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.title).toBe("Yoga");
+  });
+});
 
